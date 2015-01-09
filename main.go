6249ecba5b167c24adcd83d0c0a99461edb10242
sweeney <martin@sweeney.io>
2015-01-09 13:29:16 +0000

    Normal ping, and just ping

--- main.go
+++ main.go
@@ -6,13 +6,8 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/_hw", hw)
-	http.HandleFunc("/_ping", ping)
+	http.HandleFunc("/ping", ping)
 	http.ListenAndServe(":8080", nil)
-}
-
-func hw(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "helloworld")
 }
 
 func ping(w http.ResponseWriter, r *http.Request) {
